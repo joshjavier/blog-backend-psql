62b7cd6b7d8a0119310acc5003433b762f3a5688
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-03 18:19:23 +0800

    Exercise 13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,24 +3,14 @@ const { Blog } = require('../models')
 const router = require('express').Router()
 
 router.get('/', async (req, res) => {
-  try {
-    const blogs = await Blog.findAll()
-    res.json(blogs)
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({ error })
-  }
+  const blogs = await Blog.findAll()
+  res.json(blogs)
 })
 
 router.post('/', async (req, res) => {
   const { author, url, title, likes } = req.body
-  try {
-    const blog = await Blog.create({ author, url, title, likes })
-    res.send(blog)
-  } catch (error) {
-    console.log(error)
-    res.status(400).send({ error })
-  }
+  const blog = await Blog.create({ author, url, title, likes })
+  res.send(blog)
 })
 
 const blogFinder = async (req, res, next) => {
@@ -29,28 +19,19 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.delete('/:id', blogFinder, async (req, res) => {
-  try {
-    await req.blog?.destroy()
-    res.sendStatus(204)
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({ error })
-  }
+  await req.blog?.destroy()
+  res.sendStatus(204)
 })
 
 router.put('/:id', blogFinder, async (req, res) => {
   const { likes } = req.body
-  try {
-    if (req.blog) {
-      if (likes) req.blog.likes = likes
-      await req.blog.save()
-      res.status(200).json(req.blog)
-    } else {
-      return res.status(404).json({ error: 'Blog not found' })
-    }
-  } catch (error) {
-    console.log(error)
-    res.status(400).send({ error })
+
+  if (req.blog) {
+    if (likes) req.blog.likes = likes
+    await req.blog.save()
+    res.status(200).json(req.blog)
+  } else {
+    return res.status(404).json({ error: 'Blog not found' })
   }
 })
 
---
+++ util/middleware.js
@@ -0,0 +1,19 @@
+const errorHandler = (err, req, res, next) => {
+  console.log(err)
+
+  if (err.name === 'SequelizeValidationError') {
+    const errorObj = {}
+    err.errors.forEach(e => {
+      errorObj[e.path] = e.message
+    });
+    res.status(400).json(errorObj)
+  } else {
+    res.status(400).json({ error: err.message })
+  }
+
+  next(err)
+}
+
+module.exports = {
+  errorHandler
+}
