b85864d229b83bbca4fc5cd476eb56152ba6fdb0
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-03 11:06:32 +0800

    Exercise 13.4

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, DataTypes } = require('sequelize')
+const express = require('express')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
+const app = express()
+app.use(express.json())
 
-const main = async () => {
+const Blog = sequelize.define('blog', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  author: DataTypes.STRING,
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+}, {
+  timestamps: false,
+  underscored: true,
+})
+
+app.get('/api/blogs', async (req, res) => {
   try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
+    const blogs = await Blog.findAll()
+    res.json(blogs)
   } catch (error) {
-    console.log('Unable to connect to the database:', error)
+    console.log(error)
+    res.status(500).send({ error })
   }
-}
+})
 
-main()
+app.post('/api/blogs', async (req, res) => {
+  const { author, url, title, likes } = req.body
+  try {
+    const blog = await Blog.create({ author, url, title, likes })
+    res.send(blog)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    await blog?.destroy()
+    res.sendStatus(204)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
