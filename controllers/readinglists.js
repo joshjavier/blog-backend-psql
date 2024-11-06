204d86718f5d55b7f3465319ada874d3b8688889
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-06 13:54:18 +0800

    Exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,4 +1,5 @@
 const { ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 const router = require('express').Router()
 
@@ -6,6 +7,21 @@ router.post('/', async (req, res) => {
   const { blogId, userId } = req.body
   const readinglist = await ReadingList.create({ blogId, userId })
   res.json(readinglist)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readinglist = await ReadingList.findByPk(req.params.id)
+
+  if (!readinglist)
+    return res.sendStatus(404)
+
+  if (req.decodedToken.id === readinglist.userId) {
+    readinglist.read = req.body.read
+    await readinglist.save()
+    res.json(readinglist)
+  } else {
+    res.status(401).json({ error: 'You can only change your own reading list.' })
+  }
 })
 
 module.exports = router
